97395a96d07cf716a83ca33de77e8de4333ec5ae
Njoguu <njoguuallan@gmail.com>
2021-11-21 17:24:51 +0300

    Added FireBase to App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,22 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { FormsModule } from '@angular/forms';
+
+// import { AngularFireModule } from '@angular/fire';
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDZ-ESNCv9voIBgF5sOhiGj0bCtVHXlhxo",
+  authDomain: "web-portfolio-msgs.firebaseapp.com",
+  projectId: "web-portfolio-msgs",
+  storageBucket: "web-portfolio-msgs.appspot.com",
+  messagingSenderId: "832142751030",
+  appId: "1:832142751030:web:e238cc9945b4fc703aaf97"
+};
+
+const app = initializeApp(firebaseConfig);
 
 @NgModule({
   declarations: [
@@ -14,7 +30,9 @@ import { HomepageComponent } from './homepage/homepage.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    // AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
