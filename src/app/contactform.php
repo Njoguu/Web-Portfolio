df204e55b6055ac8388ec2420c641bd6a8c563a0
Njoguu <njoguuallan@gmail.com>
2022-03-01 22:41:15 +0300

    contact form backend

--- src/app/contactform.php
+++ src/app/contactform.php
@@ -1,19 +1,21 @@
 <?php
 
+if (isset($_POST["submit"])){
+    //get data from form  
+    $name = $_POST['name'];
+    $email= $_POST['email'];
+    $phoneNo = $_POST['phone'];
+    $message= $_POST['message'];
+    $to = "bigboydevelops@gmail.com";
+    $subject = "Test Mail From website";
 
-//get data from form  
-$name = $_POST['name'];
-$email= $_POST['email'];
-$phoneNo = $_POST['phone'];
-$message= $_POST['message'];
-$to = "bigboydevelops@gmail.com";
-$subject = "Mail From website";
+    $txt = "Email received from ".$name.".\n\n".$message;
+    $headers = "From: ".$mailFrom." Tel: ".phoneNo;
 
-$txt = "Email received from ".$name.".\n\n".$message;
-$headers = "From: ".$mailFrom." Tel: ".phoneNo;
-
-if($email!=NULL){
     mail($to,$subject,$txt,$headers);
+    
 }
+
+
 
 ?> 
